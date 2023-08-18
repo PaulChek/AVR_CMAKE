a4f357a4da0b78dd5c451b6fbd9c04e5a62ab1b9
PaulChek <paulchek777@hotmail.com>
2023-08-18 14:14:48 +0700

    font

--- src/main.c
+++ src/main.c
@@ -6,17 +6,10 @@
 #include "../spiI2cuart/ssd1306.h"
 #include "timer0.h"
 #include "adc.h"
+#include "font_small.h"
 
 volatile void rotate_bits(byte);
-static byte font[][5] = {
-    {0b01111110, 0b00010001, 0b00010001, 0b01111110, 0}, // A 4
-    {0b00111111, 0b01000000, 0b01000000, 0b00111111, 0}, // U 5
-    {0x7f, 0x08, 0x08, 0x7f, 0},                         // H 0
-    {0b00011111, 0b01100000, 0b01100000, 0b00011111, 0}, // V 6
-    {0b01000000, 0, 0, 0, 0},                            // . 2
-    {0, 0x10, 0x10, 0x10, 0},                            // - 3
-    {0x0, 0x0, 0x0, 0x0, 0},                             // space 1
-};
+
 
 int main()
 {
@@ -41,7 +34,7 @@ int main()
     {
         if (prev - timer0() >=15)
         {
-            SSD1306_DRAW(5, font[li % 7]);
+            SSD1306_DRAW(5, font[li % 18]);
             prev = timer0();
               ADCSRA|=(1<<6);
               li++;
